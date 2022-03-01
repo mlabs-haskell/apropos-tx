51fa90a832050ea44345566a4e67469ee91e714f
Fraser Dunlop <fraser@mlabs.city>
2022-03-01 16:09:00 +0000

    und

--- src/Apropos/TxArr.hs
+++ src/Apropos/TxArr.hs
@@ -1,6 +1,7 @@
 module Apropos.TxArr (
   TxArr(..),
-  (>>>>)
+  (>>>>),
+  (<++>),
   ) where
 import Plutarch
 import Plutarch.Prelude
@@ -17,6 +18,9 @@ data TxArr a b s a' b' =
 liftTxArr :: TxArr a b s a' b' -> Term s a' -> Term s b'
 liftTxArr x = (coerceOut x) . (arr x) . (coerceIn x)
 
+und :: Term s PUnit -> Term s PUnit -> Term s PUnit
+und x y = papp (papp (plam $ \_ -> plam $ \_ -> pcon PUnit) x) y
+
 (>>>>) :: forall a b c s a' b' c' . TxArr a b s a' b' -> TxArr b c s b' c' -> TxArr a c s a' c'
 (>>>>) x y = TxArr {
                arr = ((arr y) . (arr x))
@@ -30,8 +34,8 @@ liftTxArr x = (coerceOut x) . (arr x) . (coerceIn x)
           -> Term s (a' :--> c' :--> PUnit)
     ccomp xp yp = plam $ \a' ->
                      plam $ \c' ->
-                          (papp (papp xp a') ((liftTxArr x) a'))
-                       <> (papp (papp yp ((liftTxArr x) a')) c')
+                             (papp (papp xp a') ((liftTxArr x) a'))
+                       `und` (papp (papp yp ((liftTxArr x) a')) c')
 
 (<++>) :: forall a b c d s a' b' c' d' .
    TxArr a b s a' b' -> TxArr c d s c' d' -> TxArr (a,c) (b,d) s (PTuple a' c') (PTuple b' d')
