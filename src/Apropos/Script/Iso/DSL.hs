2eb53169b967625a231c5f9393c2f60bef60e265
Fraser Dunlop <fraser@mlabs.city>
2022-03-07 21:00:17 +0000

    combinators

--- src/Apropos/Script/Iso/DSL.hs
+++ src/Apropos/Script/Iso/DSL.hs
@@ -1,9 +1,23 @@
 {-# LANGUAGE RankNTypes #-}
 
 module Apropos.Script.Iso.DSL (
-  NamedArrow(..),
-  NamedUnaryConstraint(..),
-  NamedBinaryConstraint(..),
+  arrow,
+  binaryConstraint,
+  unaryConstraint,
+  (>>>),
+  (&&&),
+  (***),
+  bflip,
+  (>||),
+  (||<),
+  (>**),
+  wye,
+  (>>|),
+  (>&|),
+
+  Arrow,
+  BinaryConstraint,
+  UnaryConstraint,
   compileArrow,
   compileBinaryConstraint,
   compileUnaryConstraint,
@@ -13,22 +27,6 @@ import Plutarch.Prelude
 import Plutarch.Lift
 import Plutarch.Api.V1.Tuple
 
-class NamedArrow arrow where
-  arrowName :: arrow s a b -> String
-  mkArrow :: PlutarchArrow s a b -> arrow s a b
-  arrow :: arrow s a b -> PlutarchArrow s a b
-
-
-class NamedUnaryConstraint constraint where
-  unaryConstraintName :: constraint s a -> String
-  mkUnaryConstraint :: PlutarchUnaryConstraint s a -> constraint s a
-  unaryConstraint :: constraint s a -> PlutarchUnaryConstraint s a
-
-class NamedBinaryConstraint constraint where
-  binaryConstraintName :: constraint s a b -> String
-  mkbinaryConstraint :: PlutarchBinaryConstraint s a b -> constraint s a b
-  binaryConstraint :: constraint s a b -> PlutarchBinaryConstraint s a b
-
 type PlutarchArrow s a b = Term s (a :--> b)
 
 -- where POpaque is a truthy value e.g. perror = False, PUnit = True
@@ -37,9 +35,59 @@ type PlutarchUnaryConstraint s domain = Term s (domain :--> POpaque)
 -- where POpaque is a truthy value e.g. perror = False, PUnit = True
 type PlutarchBinaryConstraint s domainL domainR = Term s (domainL :--> domainR :--> POpaque)
 
+arrow :: PlutarchArrow s a b -> Arrow s a b
+arrow = Arrow
+
+binaryConstraint :: PlutarchBinaryConstraint s a b -> BinaryConstraint s a b
+binaryConstraint = BinaryConstraint
+
+unaryConstraint :: PlutarchUnaryConstraint s a -> UnaryConstraint s a
+unaryConstraint = UnaryConstraint
+
+(>>>) :: Arrow s a b -> Arrow s b c -> Arrow s a c
+(>>>) = ArrowSeq
+
+(&&&) :: (PIsData bl, PIsData br, b ~ PTuple bl br, PLifted (PTuple bl br) ~ (PLifted bl, PLifted br))
+      => Arrow s a bl -> Arrow s a br -> Arrow s a b
+(&&&) = ArrowFanout
+
+(***) :: (PIsData al, PIsData ar, a ~ PTuple al ar, PLifted (PTuple al ar) ~ (PLifted al, PLifted ar)
+         ,PIsData bl, PIsData br, b ~ PTuple bl br, PLifted (PTuple bl br) ~ (PLifted bl, PLifted br))
+      => Arrow s al bl -> Arrow s ar br -> Arrow s a b
+(***) = ArrowParallel
+
+bflip :: BinaryConstraint s a b -> BinaryConstraint s b a
+bflip = BinaryConstraintFlip
+
+instance Semigroup (BinaryConstraint s a b) where
+  (<>) = BinaryConstraintAssoc
+
+(>||) :: Arrow s a c -> BinaryConstraint s c b -> BinaryConstraint s a b
+(>||) = BinaryConstraintArrowLeft
+
+(||<) :: BinaryConstraint s a c -> Arrow s b c -> BinaryConstraint s a b
+(||<) = BinaryConstraintArrowRight
+
+(>**) :: (PIsData a, PIsData b, ab ~ PTuple a b, PLifted (PTuple a b) ~ (PLifted a, PLifted b)
+         ,PIsData vl, PIsData vr, v ~ PTuple vl vr, PLifted (PTuple vl vr) ~ (PLifted vl, PLifted vr))
+      => Arrow s ab v -> BinaryConstraint s vl vr -> BinaryConstraint s a b
+(>**) = BinaryConstraintArrowParallel
+
+wye :: BinaryConstraint s a a -> UnaryConstraint s a
+wye = UnaryFromBinaryConstraint
+
+instance Semigroup (UnaryConstraint s a) where
+  (<>) = UnaryConstraintAssoc
+
+(>>|) :: Arrow s a b -> UnaryConstraint s b -> UnaryConstraint s a
+(>>|) = UnaryConstraintArrow
+
+(>&|) :: (PIsData vl, PIsData vr, v ~ PTuple vl vr, PLifted (PTuple vl vr) ~ (PLifted vl, PLifted vr))
+      => Arrow s a v -> BinaryConstraint s vl vr -> UnaryConstraint s a
+(>&|) = UnaryConstraintFanoutBinary
 
 data Arrow s a b where
-  Arrow :: NamedArrow arrow => arrow s a b -> Arrow s a b
+  Arrow :: PlutarchArrow s a b -> Arrow s a b
   ArrowSeq :: Arrow s a v -> Arrow s v b -> Arrow s a b
   ArrowFanout :: (PIsData bl, PIsData br, b ~ PTuple bl br, PLifted (PTuple bl br) ~ (PLifted bl, PLifted br))
                  => Arrow s a bl -> Arrow s a br -> Arrow s a b
@@ -48,8 +96,7 @@ data Arrow s a b where
                    => Arrow s al bl -> Arrow s ar br -> Arrow s a b
 
 data BinaryConstraint s a b where
-  BinaryConstraint :: NamedBinaryConstraint constraint
-                      => constraint s a b -> BinaryConstraint s a b
+  BinaryConstraint :: PlutarchBinaryConstraint s a b -> BinaryConstraint s a b
   BinaryConstraintFlip :: BinaryConstraint s b a -> BinaryConstraint s a b
   BinaryConstraintAssoc :: BinaryConstraint s a b -> BinaryConstraint s a b -> BinaryConstraint s a b
   BinaryConstraintArrowLeft :: Arrow s a c -> BinaryConstraint s c b -> BinaryConstraint s a b
@@ -59,8 +106,7 @@ data BinaryConstraint s a b where
                                 => Arrow s ab v -> BinaryConstraint s vl vr -> BinaryConstraint s a b
 
 data UnaryConstraint s a where
-  UnaryConstraint :: NamedUnaryConstraint constraint
-                  => constraint s a -> UnaryConstraint s a
+  UnaryConstraint :: PlutarchUnaryConstraint s a -> UnaryConstraint s a
   UnaryFromBinaryConstraint :: BinaryConstraint s a a -> UnaryConstraint s a
   UnaryConstraintAssoc :: UnaryConstraint s a -> UnaryConstraint s a -> UnaryConstraint s a
   UnaryConstraintArrow :: Arrow s a b -> UnaryConstraint s b -> UnaryConstraint s a
@@ -68,7 +114,7 @@ data UnaryConstraint s a where
                               => Arrow s a v -> BinaryConstraint s vl vr -> UnaryConstraint s a
 
 compileArrow :: Arrow s a b -> PlutarchArrow s a b
-compileArrow (Arrow a) = arrow a
+compileArrow (Arrow a) = a
 compileArrow (ArrowSeq l r) =
   plam $ \a -> papp (compileArrow r) (papp (compileArrow l) a)
 compileArrow (ArrowFanout l r) = -- this reification of the tuple can be avoided by compileUnaryConstraint
@@ -84,10 +130,10 @@ opaqueSemigroup :: Term s a -> Term s b -> Term s POpaque
 opaqueSemigroup ign ore = papp (papp (plam $ \_ign _ore -> popaque $ pcon PUnit) ign) ore
 
 compileBinaryConstraint :: BinaryConstraint s a b -> PlutarchBinaryConstraint s a b
-compileBinaryConstraint (BinaryConstraint c) = binaryConstraint c
+compileBinaryConstraint (BinaryConstraint c) = c
 compileBinaryConstraint (BinaryConstraintArrowParallel (ArrowParallel l r) c) = -- this avoids the tuple reification
   compileBinaryConstraint $ BinaryConstraintArrowRight (BinaryConstraintArrowLeft l c) r
-compileBinaryConstraint (BinaryConstraintArrowParallel arr c) = --TODO rewrite arrow to ArrowParallel if possible
+compileBinaryConstraint (BinaryConstraintArrowParallel arr c) = --TODO rewrite arrow to ArrowParallel if possible via normalisation
   plam $ \a ->
     plam $ \b ->
       let tup = papp (compileArrow arr) (papp (papp ptuple (pdata a)) (pdata b))
@@ -111,7 +157,7 @@ compileBinaryConstraint (BinaryConstraintArrowRight c arr) =
       papp (papp (compileBinaryConstraint c) a) (papp (compileArrow arr) b)
 
 compileUnaryConstraint :: UnaryConstraint s a -> PlutarchUnaryConstraint s a
-compileUnaryConstraint (UnaryConstraint c) = unaryConstraint c
+compileUnaryConstraint (UnaryConstraint c) = c
 compileUnaryConstraint (UnaryConstraintFanoutBinary (ArrowFanout l r) c) = -- this avoids the tuple reification
   compileUnaryConstraint (UnaryFromBinaryConstraint (BinaryConstraintArrowRight (BinaryConstraintArrowLeft l c) r))
 compileUnaryConstraint (UnaryFromBinaryConstraint c) =
@@ -120,7 +166,7 @@ compileUnaryConstraint (UnaryConstraintAssoc l r) =
   plam $ \a -> opaqueSemigroup (papp (compileUnaryConstraint l) a) (papp (compileUnaryConstraint r) a)
 compileUnaryConstraint (UnaryConstraintArrow arr c) =
   plam $ \a -> papp (compileUnaryConstraint c) (papp (compileArrow arr) a)
-compileUnaryConstraint (UnaryConstraintFanoutBinary arr c) = --TODO rewrite arr to ArrowFanout if possible
+compileUnaryConstraint (UnaryConstraintFanoutBinary arr c) = --TODO rewrite arr to ArrowFanout if possible via normalisation
   plam $ \a ->
     let tup = papp (compileArrow arr) a
         bip = pfromData $ pbuiltinPairFromTuple $ pdata tup
