c5fe6bb76b336d6ec099dd1acc747bd007adadf8
Fraser Dunlop <fraser@mlabs.city>
2022-03-17 18:48:34 +0000

    label inputs

--- src/Plutarch/Wires.hs
+++ src/Plutarch/Wires.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE PolyKinds #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 module Plutarch.Wires (
+  -- examples
   eqNode,
   notNode,
   notEqDag,
@@ -14,30 +15,11 @@ import Plutarch.Prelude
 import GHC.TypeLits
 import Data.Proxy
 
-type PNode :: [PType] -> [PLabeledType] -> PType
+type PNode :: [PLabeledType] -> [PLabeledType] -> PType
 
 type family PNode is os where
-  PNode (i ': is) os = i :--> (PNode is os)
+  PNode ((_ ':= i) ': is) os = i :--> (PNode is os)
   PNode '[] os = PDataRecord os
-
-eqNode :: forall i. PEq i => WiringDiagram '[i,i] '["isEq" ':= PBool]
-eqNode = Node "eq" $ plam $ \a b -> pdcons @"isEq" @PBool # (pdata (a #== b)) # pdnil
-
-notNode :: WiringDiagram '[PBool] '["negated" ':= PBool]
-notNode = Node "not" $ plam $ \a -> pdcons @"negated" @PBool # (pdata (papp pnot a)) # pdnil
-
-notEqDag :: forall i. PEq i => WiringDiagram '[i,i] '["negated" ':= PBool]
-notEqDag = Wire (Proxy :: Proxy "isEq") (Proxy :: Proxy 0) eqNode notNode
-
-notEqDagBothExposed :: forall i. PEq i => WiringDiagram '[i,i] '["isEq" ':= PBool, "negated" ':= PBool]
-notEqDagBothExposed = Wyre (Proxy :: Proxy "isEq") (Proxy :: Proxy 0) eqNode notNode
-
-type ConsumeIndex :: Nat -> [a] -> [a]
-
-type family ConsumeIndex n ps where
-  ConsumeIndex 0 (_ ': ps) = ps
-  ConsumeIndex n (p ': ps) = p ': (ConsumeIndex (n - 1) ps)
-  ConsumeIndex _ '[] = '[]
 
 type Consume :: Symbol -> [PLabeledType] -> [PLabeledType]
 type family Consume s ps where
@@ -59,8 +41,24 @@ type family Index i as where
 
 data WiringDiagram is os where
   Node :: String -> Term s (PNode is os) -> WiringDiagram is os
-  Wyre :: (is ~ (Concat isa (ConsumeIndex i isb)), os ~ (Concat osa osb))
+  Wyre :: (is ~ (Concat isa (Consume i isb)), os ~ (Concat osa osb))
        => Proxy o -> Proxy i -> WiringDiagram isa osa -> WiringDiagram isb osb -> WiringDiagram is os
-  Wire :: (is ~ (Concat isa (ConsumeIndex i isb)), os ~ (Concat (Consume o osa) osb))
+  Wire :: (is ~ (Concat isa (Consume i isb)), os ~ (Concat (Consume o osa) osb))
        => Proxy o -> Proxy i -> WiringDiagram isa osa -> WiringDiagram isb osb -> WiringDiagram is os
+
+-- examples
+
+eqNode :: forall i. PEq i => WiringDiagram '["eqL" ':= i, "eqR" ':= i] '["isEq" ':= PBool]
+eqNode = Node "eq" $ plam $ \a b -> pdcons @"isEq" @PBool # (pdata (a #== b)) # pdnil
+
+notNode :: WiringDiagram '["negate" ':= PBool] '["negated" ':= PBool]
+notNode = Node "not" $ plam $ \a -> pdcons @"negated" @PBool # (pdata (papp pnot a)) # pdnil
+
+notEqDag :: forall i. PEq i => WiringDiagram '["eqL" ':= i, "eqR" ':= i]  '["negated" ':= PBool]
+notEqDag = Wire (Proxy :: Proxy "isEq") (Proxy :: Proxy "negate") eqNode notNode
+
+notEqDagBothExposed :: forall i. PEq i
+                    => WiringDiagram '["eqL" ':= i, "eqR" ':= i] '["isEq" ':= PBool, "negated" ':= PBool]
+notEqDagBothExposed = Wyre (Proxy :: Proxy "isEq") (Proxy :: Proxy "negate") eqNode notNode
+
 
